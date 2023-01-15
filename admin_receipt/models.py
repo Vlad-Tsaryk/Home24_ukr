9047f6a8389f29020793b5eacc82b3159cf261dc
Vlad Tsaryk <vladislavcarik2014@gmail.com>
2023-01-15 22:53:32 +0200

    Receipt

--- admin_receipt/models.py
+++ admin_receipt/models.py
@@ -1,9 +1,29 @@
 from django.db import models
 from admin_apartment.models import Apartment
 from admin_tariff.models import Tariff
+from admin_service.models import Service
 
 
 # Create your models here.
-# class Receipt(models.Model):
-#     apartment = models.ForeignKey(Apartment, on_delete=models.CASCADE)
-#     tariff = models.ForeignKey(Tariff, on_delete=)
+class Receipt(models.Model):
+    class StatusName(models.TextChoices):
+        PAID = 'Оплачена', 'Оплачена'
+        PRE_PAID = 'Частично оплачена', 'Частично оплачена'
+        NOT_PAID = 'Не оплачена', 'Не оплачена'
+
+    apartment = models.ForeignKey(Apartment, on_delete=models.CASCADE)
+    tariff = models.ForeignKey(Tariff, null=True, on_delete=models.SET_NULL)
+    is_complete = models.BooleanField(default=True)
+    date = models.DateField()
+    number = models.CharField(max_length=50, unique=True)
+    status = models.CharField(max_length=20, choices=StatusName.choices)
+    period_start = models.DateField()
+    period_end = models.DateField()
+    services = models.ManyToManyField(Service, through='ReceiptService')
+
+
+class ReceiptService(models.Model):
+    receipt_id = models.ForeignKey(Receipt, on_delete=models.CASCADE)
+    service_id = models.ForeignKey(Service, on_delete=models.CASCADE)
+    consumption = models.FloatField()
+    price_unit = models.FloatField()
