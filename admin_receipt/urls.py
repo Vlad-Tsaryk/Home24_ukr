bdf41bec650e7173c080733a509c00b2b330e6ef
Vlad Tsaryk <vladislavcarik2014@gmail.com>
2023-01-27 02:18:11 +0200

    Add delete url

--- admin_receipt/urls.py
+++ admin_receipt/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import ReceiptCreate, ReceiptList, ReceiptUpdate, ReceiptView, ReceiptClone
+from .views import ReceiptCreate, ReceiptList, ReceiptUpdate, ReceiptView, ReceiptClone, receipt_delete
 urlpatterns = [
     path('', ReceiptList.as_view(), name='receipt_list'),
     path('create/', ReceiptCreate.as_view(), name='receipt_create'),
     path('update/<pk>', ReceiptUpdate.as_view(), name='receipt_update'),
     path('clone/<pk>', ReceiptClone.as_view(), name='receipt_clone'),
+    path('delete/<pk>', receipt_delete, name='receipt_delete'),
     path('<pk>', ReceiptView.as_view(), name='receipt_view'),
 
 
