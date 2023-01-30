870472f24c0b2c8656ead53348865c0cc44c7347
Vlad Tsaryk <vladislavcarik2014@gmail.com>
2023-01-30 02:07:13 +0200

    Add methods for statistic

--- admin_personal_account/models.py
+++ admin_personal_account/models.py
@@ -1,10 +1,39 @@
 from django.db import models
 
 from admin_apartment.models import Apartment
+from admin_receipt.models import Receipt
 
 
 # Create your models here.
 class PersonalAccount(models.Model):
+    @property
+    def balance(self):
+        result = self.transaction_set.all().filter(is_complete=True).aggregate(models.Sum('sum'))['sum__sum']
+        for i in self.apartment.receipt_set.all().filter(is_complete=True).exclude(status=Receipt.StatusName.PAID):
+            result -= i.total_price
+        if result:
+            return result
+        else:
+            return 0
+
+    @staticmethod
+    def total_debt():
+        result = 0
+        for personal_account in PersonalAccount.objects.all():
+            balance = personal_account.balance
+            if balance < 0:
+                result += balance
+        return '%.2f' % result
+
+    @staticmethod
+    def total_balance():
+        result = 0
+        for personal_account in PersonalAccount.objects.all():
+            balance = personal_account.balance
+            if balance > 0:
+                result += balance
+        return '%.2f' % result
+
     class StatusName(models.TextChoices):
         ACTIVE = 'Активен', 'Активен'
         DISABLED = 'Неактивен', 'Неактивен'
@@ -15,11 +44,3 @@ class PersonalAccount(models.Model):
 
     def __str__(self):
         return self.number
-
-    @property
-    def balance(self):
-        result = self.transaction_set.all().filter(is_complete=True).aggregate(models.Sum('sum'))['sum__sum']
-        if result:
-            return result
-        else:
-            return 0
