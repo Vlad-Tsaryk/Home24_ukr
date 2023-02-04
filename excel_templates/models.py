61e72e3e1af4d1d66438e27623732bb0b2a81231
Vlad Tsaryk <vladislavcarik2014@gmail.com>
2023-02-05 01:07:29 +0200

    Set upload directory for file

--- excel_templates/models.py
+++ excel_templates/models.py
@@ -4,5 +4,5 @@ from django.db import models
 # Create your models here.
 class ExcelTemplate(models.Model):
     default = models.BooleanField(default=False)
-    file = models.FileField()
+    file = models.FileField(upload_to='excel_templates')
     name = models.CharField(max_length=100)
