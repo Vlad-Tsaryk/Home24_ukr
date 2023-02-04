66741821eb104c7f7c6b16a5dcb976837ee4bfc2
Vlad Tsaryk <vladislavcarik2014@gmail.com>
2023-02-05 01:06:34 +0200

    Add ExcelTemplatePrint

--- excel_templates/urls.py
+++ excel_templates/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ExcelTemplateCreate, ExcelTemplateDelete, ExcelTemplateSetDefault
+from .views import ExcelTemplateCreate, ExcelTemplateDelete, ExcelTemplateSetDefault, ExcelTemplatePrint
 urlpatterns = [
     path('', ExcelTemplateCreate.as_view(), name='excel-template-create'),
+    path('print/<pk>', ExcelTemplatePrint.as_view(), name='excel-template-print'),
     path('delete/<pk>', ExcelTemplateDelete.as_view(), name='excel-template-delete'),
     path('set_default/<pk>', ExcelTemplateSetDefault.as_view(), name='excel-template-set-default'),
 ]
--- excel_templates/views.py
+++ excel_templates/views.py
@@ -1,11 +1,16 @@
 from django.contrib import messages
 from django.contrib.messages.views import SuccessMessageMixin
+from django.http import HttpResponse
 from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.views import View
-from django.views.generic import CreateView, DeleteView
+from django.views.generic import CreateView, DeleteView, TemplateView
 from django.views.generic.detail import SingleObjectMixin
+from openpyxl.reader.excel import load_workbook
+from openpyxl.writer.excel import save_virtual_workbook
 
+from admin_purpose.models import PaymentDetails
+from admin_receipt.models import Receipt
 from excel_templates.models import ExcelTemplate
 from .forms import ExcelTemplateCreateForm
 
@@ -57,3 +62,45 @@ class ExcelTemplateSetDefault(SingleObjectMixin, View):
         obj.save()
         messages.info(self.request, f'Шаблон {obj.name} задан по умолчанию')
         return redirect('excel-template-create')
+
+
+class ExcelTemplatePrint(SingleObjectMixin, TemplateView):
+    model = Receipt
+    template_name = 'excel_templates/excel_template_print.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(SingleObjectMixin, self).get_context_data(**kwargs)
+        context['excel_template_list'] = ExcelTemplate.objects.all()
+        return context
+
+    def post(self, request, *args, **kwargs):
+        obj = self.get_object()
+        # obj = Receipt.objects.get()
+        print('hello')
+        excel_template = ExcelTemplate.objects.get(pk=self.request.POST.get('template_id')).file
+        print(excel_template)
+        print(obj)
+        account_balance = obj.apartment.personalaccount.balance
+        template_selectors = {
+            '$payCompany$': PaymentDetails.objects.first().name,
+            '$receiptAddress$': obj.address_for_excel,
+            '$receiptNumber$': obj.number,
+            '$accountNumber$': obj.apartment.personalaccount.number,
+            '$receiptDate$': obj.date.strftime('%d.%m.%Y'),
+            '$receiptMonth$': obj.date.month,
+            '$accountBalance$': account_balance,
+            '$receiptPayable$': account_balance - obj.total_price,
+            '$total$': obj.total_price,
+        }
+        wb = load_workbook(excel_template)
+        ws = wb.active
+        for row in ws.iter_rows():
+            for cell in row:
+                val = cell.value
+                if val in template_selectors:
+                    cell.value = template_selectors[val]
+        # wb.save('test.xlsx')
+        response = HttpResponse(save_virtual_workbook(wb),
+                                content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+        response['Content-Disposition'] = 'attachment; filename=export.xlsx'
+        return response
