2868a6ef0297e0210c2c0cccd030f39fd7d1e90a
Vlad Tsaryk <vladislavcarik2014@gmail.com>
2023-02-06 19:34:40 +0200

    Setting receipt_service rows to excel

--- excel_templates/views.py
+++ excel_templates/views.py
@@ -1,3 +1,6 @@
+from copy import copy, deepcopy
+from datetime import datetime
+
 from django.contrib import messages
 from django.contrib.messages.views import SuccessMessageMixin
 from django.http import HttpResponse
@@ -7,10 +10,13 @@ from django.views import View
 from django.views.generic import CreateView, DeleteView, TemplateView
 from django.views.generic.detail import SingleObjectMixin
 from openpyxl.reader.excel import load_workbook
+from openpyxl.utils import get_column_letter
+from openpyxl.worksheet.cell_range import CellRange
 from openpyxl.writer.excel import save_virtual_workbook
+from openpyxl.cell import Cell, MergedCell
 
 from admin_purpose.models import PaymentDetails
-from admin_receipt.models import Receipt
+from admin_receipt.models import Receipt, ReceiptService
 from excel_templates.models import ExcelTemplate
 from .forms import ExcelTemplateCreateForm
 
@@ -70,37 +76,142 @@ class ExcelTemplatePrint(SingleObjectMixin, TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(SingleObjectMixin, self).get_context_data(**kwargs)
-        context['excel_template_list'] = ExcelTemplate.objects.all()
+        context['excel_template_list'] = ExcelTemplate.objects.all().order_by('-pk')
         return context
 
     def post(self, request, *args, **kwargs):
         obj = self.get_object()
-        # obj = Receipt.objects.get()
         print('hello')
         excel_template = ExcelTemplate.objects.get(pk=self.request.POST.get('template_id')).file
         print(excel_template)
         print(obj)
+        ru_months = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь',
+                     'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь']
         account_balance = obj.apartment.personalaccount.balance
-        template_selectors = {
+        receipt_selectors = {
             '$payCompany$': PaymentDetails.objects.first().name,
             '$receiptAddress$': obj.address_for_excel,
             '$receiptNumber$': obj.number,
             '$accountNumber$': obj.apartment.personalaccount.number,
             '$receiptDate$': obj.date.strftime('%d.%m.%Y'),
-            '$receiptMonth$': obj.date.month,
+            '$receiptMonth$': f'{ru_months[obj.date.month - 1]} {obj.date.year}',
             '$accountBalance$': account_balance,
             '$receiptPayable$': account_balance - obj.total_price,
             '$total$': obj.total_price,
         }
+        receipt_services = obj.receiptservice_set.all()
+
+        def get_receipt_service_selectors(selector, receipt_service):
+            receipt_service_selectors = {
+                '$serviceName$': receipt_service.service.name,
+                '$servicePrice$': receipt_service.price_unit,
+                '$serviceAmount$': receipt_service.consumption,
+                '$serviceUnit$': receipt_service.service.unit.name,
+                '$serviceTotal$': receipt_service.price_unit * receipt_service.consumption,
+            }
+            return receipt_service_selectors.get(selector) or selector
+
         wb = load_workbook(excel_template)
         ws = wb.active
+        loop_coord = None
         for row in ws.iter_rows():
             for cell in row:
                 val = cell.value
-                if val in template_selectors:
-                    cell.value = template_selectors[val]
-        # wb.save('test.xlsx')
+                if val in receipt_selectors:
+                    cell.value = receipt_selectors[val]
+                elif val == '$LOOP 1$':
+                    loop_coord = cell.row
+        aaa = ws[loop_coord + 1]
+        # ws.move_range("A10:O10", rows=5, cols=1, translate=True)
+        print(aaa)
+        row_number = receipt_services.count()
+        ws.insert_rows(loop_coord + 2, amount=row_number-1)
+        cell_to_marge = None
+        merged_cell = None
+        print(aaa)
+        ws.delete_rows(loop_coord)
+        for row in range(1, row_number):
+            for index, cell in enumerate(aaa):
+                new_cell = ws.cell(row=cell.row + row, column=cell.column, value=cell.value)
+                if cell.has_style:
+                    new_cell._style = copy(cell._style)
+                if isinstance(cell, MergedCell):
+                    merged_cell = f'{get_column_letter(cell.column)}{cell.row + row}'
+                    if index == len(aaa) - 1:
+                        ws.merge_cells(f'{cell_to_marge}:{merged_cell}')
+                else:
+                    if cell_to_marge and merged_cell:
+                        print(f'{cell_to_marge}:{merged_cell}')
+                        ws.merge_cells(f'{cell_to_marge}:{merged_cell}')
+                    cell_to_marge = f'{get_column_letter(cell.column)}{cell.row + row}'
+
+        for row, receipt_service in zip(ws.iter_rows(min_row=loop_coord, max_row=loop_coord + row_number),
+                                        receipt_services):
+            for cell in row:
+                if isinstance(cell, Cell):
+                    cell.value = get_receipt_service_selectors(cell.value, receipt_service)
+
+                # print(merged_cell.coordinate)
+        # area = CellRange("A1:O1")  # area being copied
+        # for row, copy_cell in zip(ws.iter_rows(min_row=15, max_col=len(aaa), max_row=15), aaa):
+        #     for cell_a in row:
+        #         print(copy_cell.value)
+        #         cell_a = deepcopy(copy_cell)
+        #         cell_a.row = 15
+        #         # ws.margedcell
+        #         print(cell_a)
+
+        # def copy_cell(source_cell, coord, tgt):
+        #     tgt[coord].value = source_cell.value
+        #     if source_cell.has_style:
+        #         tgt[coord]._style = deepcopy(source_cell._style)
+        #     return tgt[coord]
+        #
+        # copy_cell(ws['B10'], 'A15', ws)
+        # for mcr in ws.merged_cells:
+        #     if mcr.coord not in area:
+        #         continue
+        #     print(mcr)
+        #     cr = CellRange(mcr.coord)
+        #     cr.shift(row_shift=10)
+        #     ws.merge_cells(cr.coord)
+        if loop_coord:
+            a = ws[loop_coord + 1]
+            # ws.insert_rows(loop_coord + 2, loop_coord + 5)
+            # ws.insert_rows(10, amount=5)
+            # merged_cells_range = ws.merged_cell_ranges
+            # print(merged_cells_range)
+            # test_merg = []
+            # for marg in merged_cells_range:
+            #     test_merg.append(marg.format)
+            # for merged_cell in a:
+            #     print(type(merged_cell))
+            #     if isinstance(merged_cell, MergedCell):
+            #         # if merged_cell in merged_cells_range:
+            #         #     print('asdasd')
+            #         print(merged_cell)
+            #         print(merged_cell.column)
+            #         print(merged_cell.row)
+            #         copy(merged_cell)
+            #         MergedCell(ws, row=merged_cell.row+5, column=merged_cell.column)
+            #         print('--------------------')
+            #     print(isinstance(merged_cell, MergedCell))
+            #     print(merged_cell)
+            #     merged_cell.shift(0, 3)
+            # ws.insert_rows(loop_coord+3, 3)
+            # for row in range(4):
+            #     # ws.insert_rows(loop_coord + row)
+            #     print('hello')
+            #     for c, cell in zip(range(1, ws.max_column + 1), a):
+            #         new_cell = ws.cell(row=loop_coord+row, column=c, value=cell.value)
+            #         if cell.has_style:
+            #             new_cell._style = copy(cell._style)
+            # ws[loop_coord].append(ws[loop_coord])
+            print(loop_coord)
+            # ws[loop_coord + 1] = ws[loop_coord]
         response = HttpResponse(save_virtual_workbook(wb),
                                 content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
-        response['Content-Disposition'] = 'attachment; filename=export.xlsx'
+        response['Content-Disposition'] = f'attachment; filename=receipt__{receipt_selectors["$receiptNumber$"]}_' \
+                                          f'{receipt_selectors["$receiptDate$"]}.xlsx'
+        print(response['Content-Disposition'])
         return response
