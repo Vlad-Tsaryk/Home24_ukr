0d194968f293fff2dd7af35be7c0657d4c273242
Vlad Tsaryk <vladislavcarik2014@gmail.com>
2023-02-07 19:58:58 +0200

    Add file validation

--- excel_templates/forms.py
+++ excel_templates/forms.py
@@ -1,12 +1,16 @@
 from django import forms
 
 from excel_templates.models import ExcelTemplate
+from django.core.validators import FileExtensionValidator
 
 
 class ExcelTemplateCreateForm(forms.ModelForm):
     name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}), required=False)
+    file = forms.FileField(widget=forms.FileInput(attrs={
+        'accept': 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel'}),
+        validators=[FileExtensionValidator(allowed_extensions=["xlsx"], message='Только файлы MicrosoftExcel')])
 
     class Meta:
         model = ExcelTemplate
-        fields = '__all__'
+        fields = ['name', 'file']
         exclude = ['default', ]
