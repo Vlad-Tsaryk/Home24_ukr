d515d6e0dd1d6944fbe2bce8c7d3775ab9f4e1e8
Vlad Tsaryk <vladislavcarik2014@gmail.com>
2023-02-23 00:09:18 +0200

    Add redirects and breadcrumbs

--- cabinet_summary/urls.py
+++ cabinet_summary/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ApartmentSummary
+from .views import ApartmentSummary, ApartmentSummaryRedirect
 
 urlpatterns = [
     path('apartment_id=<pk>', ApartmentSummary.as_view(), name='cabinet_apartment_summary'),
+    path('', ApartmentSummaryRedirect.as_view(), name='cabinet'),
 ]
