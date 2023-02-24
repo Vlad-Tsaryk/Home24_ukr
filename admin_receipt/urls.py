48652875d17d19cde6f3e55e24bd540a3ac37962
Vlad Tsaryk <vladislavcarik2014@gmail.com>
2023-02-24 02:29:32 +0200

    Add  view by apartment or personal_account for receipts, meters, income transactions. Add links to create by personal account, receipt  and income transaction

--- admin_receipt/urls.py
+++ admin_receipt/urls.py
@@ -2,7 +2,9 @@ from django.urls import path, include
 from .views import ReceiptCreate, ReceiptList, ReceiptUpdate, ReceiptView, ReceiptClone, receipt_delete
 urlpatterns = [
     path('', ReceiptList.as_view(), name='receipt_list'),
+    path('apartment_number=<apartment>', ReceiptList.as_view(), name='receipt_list_apartment'),
     path('create/', ReceiptCreate.as_view(), name='receipt_create'),
+    path('create/personal_account=<personal_account>', ReceiptCreate.as_view(), name='receipt_create_personal_account'),
     path('update/<pk>', ReceiptUpdate.as_view(), name='receipt_update'),
     path('clone/<pk>', ReceiptClone.as_view(), name='receipt_clone'),
     path('delete/<pk>', receipt_delete, name='receipt_delete'),
--- admin_transaction/urls.py
+++ admin_transaction/urls.py
@@ -4,10 +4,13 @@ from .views import TransactionCreate, TransactionList, TransactionView, Transact
 
 urlpatterns = [
     path('', TransactionList.as_view(), name='transaction_list'),
+    path('personal_account=<personal_account>', TransactionList.as_view(), name='transaction_list_personal_account'),
     path('<pk>', TransactionView.as_view(), name='transaction_view'),
     path('update/<pk>', TransactionUpdate.as_view(), name='transaction_update'),
     path('clone/<pk>', TransactionClone.as_view(), name='transaction_clone'),
     path('create/type=<transaction_type>', TransactionCreate.as_view(), name='transaction_create'),
+    path('create/type=<transaction_type>/personal_account_id=<personal_account_id>', TransactionCreate.as_view(),
+         name='transaction_create_personal_account'),
     path('delete/<pk>', TransactionDelete.as_view(), name='transaction_delete'),
     path('to_excel/<pk>', TransactionToExcel.as_view(), name='transaction_to_excel'),
 ]
