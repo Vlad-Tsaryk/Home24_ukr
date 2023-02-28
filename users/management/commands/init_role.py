e445fe7bd5197b8a8e8ff93629ab2b45a561f8ab
Vlad Tsaryk <vladislavcarik2014@gmail.com>
2023-02-28 15:31:10 +0200

    Init scrips

--- users/management/commands/init_role.py
+++ users/management/commands/init_role.py
@@ -1,13 +1,9 @@
 from django.core.management.base import BaseCommand
-from users.models import User, Role
-from faker import Faker
-from random import choice
-from phonenumber_field.phonenumber import PhoneNumber
+from users.models import Role
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        fake = Faker()
         if not Role.objects.exists():
             for director_manger in Role.RoleName.values[:2]:
                 Role.objects.create(role=director_manger,
@@ -28,7 +24,7 @@ class Command(BaseCommand):
                                     users=True,
                                     payment_details=True)
 
-            for electrician_plumber in Role.RoleName.choices[:-2]:
+            for electrician_plumber in Role.RoleName.values[:-2]:
                 Role.objects.create(role=electrician_plumber,
                                     statistics=False,
                                     transactions=False,
@@ -82,29 +78,3 @@ class Command(BaseCommand):
                                 roles=False,
                                 users=False,
                                 payment_details=False)
-
-        if User.objects.count() == 0:
-            username = 'admin'
-            email = 'admin@admin.com'
-            password = 'admin'
-            print('Creating account for %s (%s)' % (username, email))
-            admin = User.objects.create_superuser(
-                username=email,
-                first_name=username,
-                middle_name=username,
-                last_name=username,
-                phone=PhoneNumber.from_string('+38 (073) 111-11-11', region="UA"),
-                viber=PhoneNumber.from_string('+38 (073) 111-11-11', region="UA"),
-                telegram='@' + 'admin',
-                birth_date=fake.date_of_birth(minimum_age=16, maximum_age=55),
-                password=password,
-                status=choice(User.StatusName.choices)[0],
-                role=Role.objects.get(role=Role.RoleName.DIRECTOR),
-                notes=fake.text(max_nb_chars=500),
-                profile_image=f'static_kit/users/admin.png'
-            )
-            admin.is_active = True
-            admin.is_admin = True
-            admin.save()
-        else:
-            print('Admin accounts can only be initialized if no Accounts exist')
