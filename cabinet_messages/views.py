6c888f5307e42c0e33f79a4f6bffd5dc63af4f42
Vlad Tsaryk <vladislavcarik2014@gmail.com>
2023-03-02 18:52:45 +0200

    Messages server side datatable

--- cabinet_messages/views.py
+++ cabinet_messages/views.py
@@ -1,3 +1,7 @@
+from django.core.paginator import Paginator
+from django.db.models import Q, Value
+from django.db.models.functions import Concat
+from django.http import JsonResponse
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 
@@ -13,6 +17,31 @@ class MessageList(OwnerPermissionRequiredMixin, ListView):
 
     def get_queryset(self):
         return Message.objects.select_related('sender').filter(receivers=self.request.user).order_by(self.ordering)
+
+    def render_to_response(self, context, **response_kwargs):
+        if self.request.is_ajax():
+            search_value = self.request.GET.get('search[value]')
+            filtered_qs = self.get_queryset().filter(Q(text__contains=search_value) | Q(subject__contains=search_value))
+            result_list = list(
+                filtered_qs
+                .annotate(sender_name=Concat('sender__first_name', Value(' '), 'sender__last_name'))
+                .values('id', 'text', 'subject', 'created', 'sender_name'))
+
+            start = int(self.request.GET.get('start', 0))
+            length = int(self.request.GET.get('length', 10))
+            paginator = Paginator(result_list, self.request.GET.get('length', 10))
+            page = (start // length) + 1
+            data = list(paginator.get_page(page))
+            print(data)
+            result = {
+                'data': data,
+                'recordsTotal': paginator.count,
+                'recordsFiltered': paginator.count,
+                'pages': paginator.num_pages,
+            }
+            return JsonResponse(result, safe=False, **response_kwargs)
+        else:
+            return super(ListView, self).render_to_response(context, **response_kwargs)
 
 
 class MessageView(OwnerPermissionRequiredMixin, DetailView):
